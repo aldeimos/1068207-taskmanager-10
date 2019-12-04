401384a77aa0053c8e81f7996d8f69090a08dedf
Alexander <al.deimosfrontend@gmail.com>
2019-12-04 22:36:38 +0300

    module4-task1

--- src/components/task.js
+++ src/components/task.js
@@ -1,5 +1,5 @@
 import {monthNames} from '../const.js';
-import {formatTime} from '../utils.js';
+import {formatTime, createElement} from '../utils.js';
 
 const createHashtagsMarkup = (hashtags) => {
   return hashtags
@@ -15,7 +15,7 @@ const createHashtagsMarkup = (hashtags) => {
     .join(`\n`);
 };
 
-export const createTaskTemplate = (task) => {
+const createTaskTemplate = (task) => {
   const {
     description,
     tags,
@@ -81,3 +81,23 @@ export const createTaskTemplate = (task) => {
     </article>`
   );
 };
+
+export default class Task {
+  constructor(task) {
+    this._task = task;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskTemplate(this._task);
+  }
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+  remoteElement() {
+    this._element = null;
+  }
+}
